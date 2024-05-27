5fcdbf82d981527fb07ea65a728575cb9fc90e71
mrkn <markiian.design@gmail.com>
2024-05-27 14:45:19 +0200

    created pages and configured navigation links

--- carzone_main/settings.py
+++ carzone_main/settings.py
@@ -118,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = ['carzone_main/static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request=request, template_name='pages/home.html')
+
+
+def about(request):
+    return render(request=request, template_name='pages/about.html')
+
+
+def services(request):
+    return render(request=request, template_name='pages/services.html')
+
+
+def contact(request):
+    return render(request=request, template_name='pages/contact.html')
